4aa7bf55a4951ef95c4caa74b5f0f0315fc90422
Salva <saruba@gmail.com>
2016-09-28 16:54:03 +0200

    add react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 
+const HelloWorld = () => {
+  return <div>
+    Hello World
+  </div>
+}
+
+ReactDom.render(
+  React.createElement(HelloWorld, null),
+  document.getElementById('app')
+)
